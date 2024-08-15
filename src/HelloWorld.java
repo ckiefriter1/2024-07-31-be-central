389bb5cfbe83f97cb5f6e001f4522b5491e207c7
Chuck Kiefriter <ckiefriter@gmail.com>
2024-08-14 21:42:28 -0400

    week2 - added HelloWorld class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,9 @@
 
 public class HelloWorld {
-	// Required to make this class an executable program.
+
 	public static void main(String[] args) {
-			
-		// Code that executes when this class is run as a program.
-		System.out.println("Hello World");
+		System.out.println("Hello world");
+
 	}
 
 }
