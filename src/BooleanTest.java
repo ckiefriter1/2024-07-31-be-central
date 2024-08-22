1eac71cdc5467cce75322935be305b24fb67689b
Chuck Kiefriter <ckiefriter@gmail.com>
2024-08-21 21:45:20 -0400

    week3

---
+++ src/BooleanTest.java
@@ -0,0 +1,12 @@
+
+public class BooleanTest {
+
+	public static void main(String[] args) {
+		boolean loggedIn = false;
+		
+		System.out.println(loggedIn);
+		System.out.println(!loggedIn);
+
+	}
+
+}
